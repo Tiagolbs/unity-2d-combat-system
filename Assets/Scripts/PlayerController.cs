77589057ccbc8add284f274527e15bb2bba609ca
Tiago Seben <tiagolbs31@gmail.com>
2023-11-08 20:33:06 -0300

    Initial setup
    Add player movement, basic physics and animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+    
+    private static readonly int MoveX = Animator.StringToHash("moveX");
+    private static readonly int MoveY = Animator.StringToHash("moveY");
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        
+        myAnimator.SetFloat(MoveX, movement.x);
+        myAnimator.SetFloat(MoveY, movement.y);
+
+    }
+
+    private void Move()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector2 mousePosition = playerControls.Movement.MousePosition.ReadValue<Vector2>();
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        mySpriteRenderer.flipX = mousePosition.x < playerScreenPoint.x;
+    }
+}
